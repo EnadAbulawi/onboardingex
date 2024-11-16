5b001eff49493a6757de0aa69780459b41f13576
EnadAbulawi <EnadAbulawi7@gmail.com>
2024-11-16 22:07:43 +0200

    .

--- lib/OnBoarding Screen/onboarding_contents.dart
+++ lib/OnBoarding Screen/onboarding_contents.dart
@@ -10,37 +10,37 @@ class OnboardingContents {
 List<OnboardingContents> contents = [
   OnboardingContents(
     title: 'أهلابك في تطبيق نور ',
-    image: 'assets/image/appiconwithoutbg.png',
+    image: '',
     description:
         'تطبيق إسلامي شامل لكل ما تحتاجه من أذكار، أدعية،ايات,وخلفيات اسلامية مصممه بعنايه لتناسب هاتفك والكثير من الميزات التي ستكتشفها داخل التطبيق ',
   ),
   OnboardingContents(
     title: "ذكرك ... طمأنينة لقلبك",
-    image: 'assets/image/doaa.png',
+    image: '',
     description:
         "احصل على أذكار الصباح والمساء وأذكار متنوعة لكل لحظة من حياتك، مرتبة وسهلة الوصول في أي وقت.",
   ),
   OnboardingContents(
     title: "آيات قرآنية بين يديك",
-    image: 'assets/image/quranverses.png',
+    image: '',
     description:
         "نقدم لك آيات مختارة من القرآن الكريم مع تفسير بسيط يساعدك على فهم كلمات الله والعمل بها.",
   ),
   OnboardingContents(
     title: "نُذكرك دائماً",
-    image: 'assets/image/daynotification.png',
+    image: '',
     description:
         "اشعارات يومية تذكرك بقراءة اذكار الصباح والمساء وتذكرك بقراءة وردك اليومي من القران الكريم واشعار كل ساعة يذكرك بالصلاة على رسول الله",
   ),
   OnboardingContents(
     title: "خلفيات إسلامية مميزة",
-    image: 'assets/image/wallpaper.png',
+    image: '',
     description:
         "اكتشف مجموعة من الخلفيات الإسلامية الرائعة التي تم تصميمها بعناية لتناسبك و لتزيين هاتفك",
   ),
   OnboardingContents(
     title: "حول النصوص الى صور",
-    image: 'assets/image/generative-image.png',
+    image: '',
     description:
         "أنشئ صورًا رائعة من النصوص التي بالتطبيق ك نصوص الادعية والايات، مع إمكانية تخصيص الألوان، نوع الخط، والخلفيات. يمكنك مشاركة الصور مباشرة او حفظها على هاتفك",
   ),
